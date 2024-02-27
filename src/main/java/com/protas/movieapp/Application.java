0726eb322a1fb71a3ddc65d20baf773908941ef2
Michał <michalprotas00@gmail.com>
2024-02-27 21:22:23 +0100

    DirectorMapper using MapStruct

--- src/main/java/com/protas/movieapp/Application.java
+++ src/main/java/com/protas/movieapp/Application.java
@@ -1,13 +1,7 @@
 package com.protas.movieapp;
 
-import com.protas.movieapp.dto.ScreeningDTO;
-import com.protas.movieapp.mapper.ScreeningMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/com/protas/movieapp/mapper/DirectorMapper.java
+++ src/main/java/com/protas/movieapp/mapper/DirectorMapper.java
@@ -2,13 +2,10 @@ package com.protas.movieapp.mapper;
 
 import com.protas.movieapp.dto.DirectorDTO;
 import com.protas.movieapp.entity.movie.Director;
+import org.mapstruct.Mapper;
 
-public class DirectorMapper {
-    public static Director fromDTO(DirectorDTO dto) {
-        Director director = new Director();
-        director.setFirstName(dto.firstName());
-        director.setLastName(dto.lastName());
-
-        return director;
-    }
+@Mapper(componentModel = "spring")
+public interface DirectorMapper {
+    Director fromDTO(DirectorDTO dto);
+    DirectorDTO toDTO(Director director);
 }
