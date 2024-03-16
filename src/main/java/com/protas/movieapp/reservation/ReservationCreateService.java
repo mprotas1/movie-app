410e081a982fcf2fd91f59525a851509fd627759
Michał <michalprotas00@gmail.com>
2024-03-16 20:15:54 +0100

    Time and Seat validation for Reservation

--- src/main/java/com/protas/movieapp/reservation/ReservationCreateService.java
+++ src/main/java/com/protas/movieapp/reservation/ReservationCreateService.java
@@ -11,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class ReservationCreateService {
@@ -26,17 +29,34 @@ public class ReservationCreateService {
         LOGGER.info("Performing reservation for user: {} for Screening: {}", customer.getId(), reservationDTO.screeningId());
         Screening screening = screeningReadService.findById(reservationDTO.screeningId());
         Seat reservationSeat = seatReadService.getSeatFromScreeningRoomAndSeatDTO(screening.getScreeningRoom(), reservationDTO.seatDTO());
+
+        Reservation reservation = Reservation.builder()
+                .withCustomer(customer)
+                .withScreening(screening)
+                .withSeat(reservationSeat)
+                .build();
+
+        validateReservation(reservation);
+
+        return mapper.toDTO(reservationRepository.save(reservation));
+    }
+
+    private void validateReservation(Reservation reservation) {
+        validateSeat(reservation.getSeat());
+        validateReservationTime(reservation.getMovieScreening());
+    }
+
+    private void validateSeat(Seat reservationSeat) {
         if(reservationReadService.reservationExistsBySeatId(reservationSeat.getId())) {
             LOGGER.info("Could not process the Reservation - the Seat [{} {}] is already taken",
                     reservationSeat.getSeatRowNumber(),
                     reservationSeat.getSeatColumnNumber());
             throw new SeatIsAlreadyTakenException("The reservation for this Seat already exists");
         }
-        Reservation reservation = Reservation.builder()
-                .withCustomer(customer)
-                .withScreening(screening)
-                .withSeat(reservationSeat)
-                .build();
-        return mapper.toDTO(reservationRepository.save(reservation));
+    }
+
+    private void validateReservationTime(Screening screening) {
+        if(LocalDateTime.now().isAfter(screening.getStartTime()))
+            throw new DateTimeException("The requested Screening for Reservation is not available");
     }
 }
